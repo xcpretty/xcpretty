d949876024654e76c4357399f971898f8693ce16
Delisa Mason <iskanamagus@gmail.com>
2013-12-13 01:23:07 -0800

    Simplify JUnit implementation

--- lib/xcpretty/reporters/junit.rb
+++ lib/xcpretty/reporters/junit.rb
@@ -22,18 +22,13 @@ module XCPretty
     def handle(line)
       case line
       when TESTS_RUN_START_MATCHER
-        @test_count = 0
-        suite = @document.add_element("testsuite")
-        suite.attributes["name"] = $1
+        create_test_suite($1)
       when TESTS_RUN_COMPLETION_MATCHER
-        current_suite.attributes["tests"] = @test_count
-        @test_count = 0
+        finish_test_suite
       when PASSING_TEST_MATCHER
         create_passing_test_case($1, $2, $3)
-        @test_count+=1
       when FAILING_TEST_MATCHER
         create_failing_test_case($2, $3, $4)
-        @test_count+=1
       end
     end
 
@@ -42,6 +37,20 @@ module XCPretty
       formatter = REXML::Formatters::Pretty.new(2)
       formatter.compact = true
       formatter.write(@document, File.open(FILEPATH,'w+'))
+    end
+
+    def create_test_suite(name)
+      @test_count = 0
+      @fail_count = 0
+      suite = @document.add_element("testsuite")
+      suite.attributes["name"] = $1
+    end
+
+    def finish_test_suite
+      current_suite.attributes["tests"] = @test_count
+      current_suite.attributes["failures"] = @fail_count
+      @test_count = 0
+      @fail_count = 0
     end
 
     def current_suite
@@ -55,6 +64,8 @@ module XCPretty
       fail_node = test_node.add_element("failure")
       fail_node.attributes["message"] = "test failure"
       fail_node.text = reason
+      @test_count+=1
+      @fail_count+=1
     end
 
     def create_passing_test_case(classname, name, time)
@@ -62,6 +73,7 @@ module XCPretty
       test_node.attributes["classname"] = classname
       test_node.attributes["name"]      = name
       test_node.attributes["time"]      = time
+      @test_count+=1
     end
   end
 end
