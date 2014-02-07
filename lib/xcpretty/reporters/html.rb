dd0d0ef000f316274e32f2f8307add56c547e3be
Delisa Mason <iskanamagus@gmail.com>
2014-02-06 22:25:57 -0800

    Add test counters

--- lib/xcpretty/reporters/html.rb
+++ lib/xcpretty/reporters/html.rb
@@ -19,6 +19,8 @@ module XCPretty
       @test_suites = {}
       @filepath    = options[:path] || FILEPATH
       @parser      = Parser.new(self)
+      @test_count  = 0
+      @fail_count  = 0
     end
 
     def handle(line)
@@ -41,14 +43,20 @@ module XCPretty
     private
 
     def add_test(suite_name, data)
+      @test_count += 1
       @test_suites[suite_name] ||= {:tests => []}
       @test_suites[suite_name][:tests] << data
-      @test_suites[suite_name][:failing] = true if data[:failing]
+      if data[:failing]
+        @test_suites[suite_name][:failing] = true
+        @fail_count += 1
+      end
     end
 
     def write_report
       File.open(@filepath, 'w') do |f|
         test_suites = @test_suites
+        fail_count  = @fail_count
+        test_count  = @test_count
         erb = ERB.new(File.open(TEMPLATE, 'r').read)
         f.write erb.result(binding)
       end
