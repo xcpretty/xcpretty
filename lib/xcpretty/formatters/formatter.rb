2227488f87a2c0209026f36268d1100eb84df45d
Vladimir Burdukov <chippcheg@gmail.com>
2015-05-14 16:46:42 +0300

    Fixes #143 - Added support for CompileStoryboard output

--- lib/xcpretty/formatters/formatter.rb
+++ lib/xcpretty/formatters/formatter.rb
@@ -18,6 +18,7 @@ module XCPretty
     def format_clean_remove;                                   EMPTY; end
     def format_compile(file_name, file_path);                  EMPTY; end
     def format_compile_command(compiler_command, file_path);   EMPTY; end
+    def format_compile_storyboard(file_name, file_path);       EMPTY; end
     def format_compile_xib(file_name, file_path);              EMPTY; end
     def format_copy_header_file(source, target);               EMPTY; end
     def format_copy_plist_file(source, target);                EMPTY; end
