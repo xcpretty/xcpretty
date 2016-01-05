2d63db53b0693ea98ceaf64163d01956fcba936d
Huy Nguyen <huy@highfive.com>
2016-01-04 18:03:19 -0800

    Address hound comments

--- spec/xcpretty/reporters/junit_spec.rb
+++ spec/xcpretty/reporters/junit_spec.rb
@@ -1,21 +1,19 @@
 require 'xcpretty'
+require 'tempfile'
 
 module XCPretty
   describe JUnit do
     before(:each) do
-      @reporterFile = Tempfile.new('junit')
-      @formatter = JUnit.new({path: @reporterFile.path})
+      @reporter_file = Tempfile.new('junit')
+      @formatter = JUnit.new(path: @reporter_file.path)
     end
 
     it "has name attribute in root node" do
       test_name = "ReactiveCocoaTests.xctest"
       @formatter.format_test_run_started(test_name)
       @formatter.finish
-
-      document  = REXML::Document.new(@reporterFile)
+      document = REXML::Document.new(@reporter_file)
       document.root.attributes['name'].should == test_name
     end
   end
 end
-
-
