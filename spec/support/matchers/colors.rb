74e3fa9b05c20751009e8c107eef7296de0bd083
Marin Usalj <marin2211@gmail.com>
2017-03-01 20:32:07 -0800

    F... rspec 3

--- spec/support/matchers/colors.rb
+++ spec/support/matchers/colors.rb
@@ -11,10 +11,10 @@ RSpec::Matchers.define :be_colored do |expected|
     applied_effects(actual).include? expected.to_sym
   end
 
-  failure_message do |actual|
+  failure_message_for_should do |actual|
     "expected that '#{strip(actual)}' would be colored #{expected}, but was #{effects_string(actual)}"
   end
-  failure_message_when_negated do |actual|
+  failure_message_for_should_not do |actual|
     "expected that #{strip(actual)} would not be colored #{expected}, but was #{effects_string(actual)}"
   end
 end
