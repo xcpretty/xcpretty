fdbdab3576b4f45e401be2f5ebd048c3450178b2
saitjr <tangjr.work@gmail.com>
2018-10-12 18:43:45 +0800

    try test again

--- spec/xcpretty/formatters/formatter_spec.rb
+++ spec/xcpretty/formatters/formatter_spec.rb
@@ -81,6 +81,17 @@ module XCPretty
 )
     end
 
+    it "formats linker undefined symbols not end with o by default" do
+      @formatter.format_undefined_symbols("Undefined symbols for architecture x86_64",
+                                          '_OBJC_CLASS_$_CABasicAnimation',
+                                          'objc-class-ref in ATZRadialProgressControl.a(ATZRadialProgressControl_A.o)').should == %Q(
+#{@formatter.red("❌  Undefined symbols for architecture x86_64")}
+> Symbol: _OBJC_CLASS_$_CABasicAnimation
+> Referenced from: objc-class-ref in ATZRadialProgressControl.a(ATZRadialProgressControl_A.o)
+
+)
+    end
+
     it "formats linker duplicate symbols by default" do
       @formatter.format_duplicate_symbols("duplicate symbol _OBJC_IVAR_$ClassName._ivarName in",
         ['/Users/username/Library/Developer/Xcode/DerivedData/App-arcyyktezaigixbocjwfhsjllojz/Build/Intermediates/App.build/Debug-iphonesimulator/App.build/Objects-normal/i386/ClassName.o',
