xcpretty/xcpretty

commit 16a96b7123fdd387a14d25f98c75cadd4d53180f

Fix unescaped HTML not being rendered properly

lib/xcpretty/parser.rb
@@ -363,7 +363,7 @@ module XCPretty
       when UI_FAILING_TEST_MATCHER
         formatter.format_failing_test(@test_suite, @test_case, $2, $1)
       when FAILING_TEST_MATCHER
-        formatter.format_failing_test($2, $3, $4, $1)
+        formatter.format_failing_test($2, $3, ERB::Util.html_escape($4), $1)
       when FATAL_ERROR_MATCHER
         formatter.format_error($1)
       when FILE_MISSING_ERROR_MATCHER

spec/fixtures/constants.rb
@@ -50,6 +50,7 @@ SAMPLE_XCTEST_FAILURE = ""
 SAMPLE_KIWI_FAILURE = "/Users/musalj/code/OSS/ObjectiveSugar/Example/ObjectiveSugarTests/NSNumberTests.m:49: error: -[NumberAdditions Iterators_TimesIteratesTheExactNumberOfTimes] : 'Iterators, times： iterates the exact number of times' [FAILED], expected subject to equal 4, got 5"
 SAMPLE_OLD_SPECTA_FAILURE = "/Users/musalj/code/OSS/ReactiveCocoa/ReactiveCocoaFramework/ReactiveCocoaTests/RACCommandSpec.m:458: error: -[RACCommandSpec enabled_signal_should_send_YES_while_executing_is_YES_and_allowsConcurrentExecution_is_YES] : expected: 1, got: 0"
 SAMPLE_SPECTA_FAILURE = "         Test Case '-[SKWelcomeViewControllerSpecSpec SKWelcomeViewController_When_a_user_opens_the_app_from_a_clean_installation_displays_the_welcome_screen]' started. \n/Users/vickeryj/Code/ipad-register/KIFTests/Specs/SKWelcomeViewControllerSpec.m:11: error: -[SKWelcomeViewControllerSpecSpec SKWelcomeViewController_When_a_user_opens_the_app_from_a_clean_installation_displays_the_welcome_screen] : The step timed out after 2.00 seconds: Failed to find accessibility element with the label \"The asimplest way to make smarter business decisions\""
+SAMPLE_SWIFT_ENUM_FAILURE = "Test Case '-[App_Tests.ComparatorSpec a_comparator__when_matching_against_test1__returns_almost_equal_for_test2]' started. \n/Users/revolt/Development/App/Comparator/ComparatorSpec.swift:76: error: -[App_Tests.ComparatorSpec a_comparator__when_matching_against_test1__returns_almost_equal_for_test2] : expected to equal <ComparisonAlmostEqual>, got <ComparisonEqual>"
 SAMPLE_SWIFT_FATAL_ERROR_IN_TEST_MAKE_TESTS_RESTARTING = %Q(\
 Test Suite 'All tests' started at 2017-07-10 15:01:12.049
 Test Suite 'xcprettyFatalErrorTests.xctest' started at 2017-07-10 15:01:12.049

spec/xcpretty/parser_spec.rb
@@ -181,7 +181,7 @@ module XCPretty
     it "parses specta failing tests" do
       @formatter.should receive(:format_failing_test).with("SKWelcomeViewControllerSpecSpec",
                                                            "SKWelcomeViewController_When_a_user_opens_the_app_from_a_clean_installation_displays_the_welcome_screen",
-                                                           "The step timed out after 2.00 seconds: Failed to find accessibility element with the label \"The asimplest way to make smarter business decisions\"",
+                                                           "The step timed out after 2.00 seconds: Failed to find accessibility element with the label &quot;The asimplest way to make smarter business decisions&quot;",
                                                            "/Users/vickeryj/Code/ipad-register/KIFTests/Specs/SKWelcomeViewControllerSpec.m:11")
       @parser.parse(SAMPLE_SPECTA_FAILURE)
     end
@@ -217,6 +217,14 @@ module XCPretty
       @formatter.should receive(:format_pending_test).with('TAPIConversationSpec',
                                                            'TAPIConversation_createConversation_SendsAPOSTRequestToTheConversationsEndpoint')
       @parser.parse(SAMPLE_PENDING_KIWI_TEST)
+    end
+
+    it "parses and escapes Swift enum names", focus: true do
+      @formatter.should receive(:format_failing_test).with('App_Tests.ComparatorSpec',
+                                                           'a_comparator__when_matching_against_test1__returns_almost_equal_for_test2',
+                                                           'expected to equal &lt;ComparisonAlmostEqual&gt;, got &lt;ComparisonEqual&gt;',
+                                                           '/Users/revolt/Development/App/Comparator/ComparatorSpec.swift:76')
+      @parser.parse(SAMPLE_SWIFT_ENUM_FAILURE)
     end
 
     it 'parses measuring tests' do

commit 228bb3a5a91c52ad25dc03db83ddedaa3f1d90d1

Fix uninitialized constant XCPretty::Parser::ERB crash
Introduced by 16a96b71.

lib/xcpretty/parser.rb
@@ -299,7 +299,15 @@ module XCPretty
 
     attr_reader :formatter
 
+    def load_dependencies
+      unless @@loaded ||= false
+        require 'erb'
+        @@loaded = true
+      end
+    end
+
     def initialize(formatter)
+      load_dependencies
       @formatter = formatter
     end
 